9637c888fe2b09c1e5a4d6420b16a36940f12508
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-02-24 11:59:40 -0500

    getting closer on having something working...

--- R/flw_get_contributions.R
+++ R/flw_get_contributions.R
@@ -1,44 +1,54 @@
 #' Get contribution data from Follow The Money
 #'
 #' This package utilizes the Follow The Money API and returns a tidy data frame
-#' of state level political contributions
+#' of political contributions.
+#'
+#' @param state
+#' @param year
+#' @param entity
+#' @param api_key
 #'
 #' @export
+#' @examples
+#' entities <- c(6404143,6480769)
+#' flw_get_contributions(entity = entities)
 flw_get_contributions <- function(state = NULL, year = NULL, entity = NULL,
                                   api_key = NULL){
   if(is.null(api_key)){
     api_key <- Sys.getenv("follow_the_money_key")
-    if(nchar(api_key == 0)){
-      stop("No Follow the Money API key found in your environment. Either\n
-           specify one directly or use flw_set_api_key() to set the\n
+    if(nchar(api_key) == 0){
+      stop("No Follow the Money API key found in your environment. Either
+           specify one directly or use flw_set_api_key() to set the
            environment variable.")
     }
   }
 
-  eid <- "http://api.followthemoney.org/?y=2014&c-t-eid=2922304&gro=d-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
-  cid <- "http://api.followthemoney.org/?c-t-id=163302&gro=d-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
-  ids <- "http://api.followthemoney.org/?s=IL&y=2014&gro=c-t-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
+  # examples
+  #eid <- "http://api.followthemoney.org/?y=2016&s=RI&gro=d-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
+  #cid <- "http://api.followthemoney.org/?c-t-id=163302&gro=d-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
+  ids <- "http://api.followthemoney.org/?s=RI&y=2016&gro=y,c-t-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json"
+  #working <- "https://api.followthemoney.org/?f-fc=2&c-t-eid=6404143&gro=y,d-eid,c-t-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json&y=2016"
 
-
-
-
-  working <- "https://api.followthemoney.org/?f-fc=2&c-t-eid=6404143&gro=y,d-eid,c-t-id&APIKey=40dc88029cfee6ac7c70142f41f895b2&mode=json&y=2016"
   url <- paste0("https://api.followthemoney.org/?mode=json", "&APIKey=", api_key)
 
   if(!is.null(state)){
-    url <- paste0(url, "&s=", state)
+    url <- paste0(url, "&s=", paste0(state, collapse = ","))
   }
 
   if(!is.null(year)){
-    url <- paste0(url, "&y=", year)
+    url <- paste0(url, "&y=", paste0(year,collapse = ","))
   }
 
   if(!is.null(entity)){
-    url <- paste0(url, "&eid=", entity)
+    url <- paste0(url, "&c-t-eid=", paste0(entity, collapse = ","))
 
   }
 
-  request<-httr::GET(ids, httr::accept("application/json"))
-  content<-httr::content(request, "text", encoding = "UTF-8")
-  records<-jsonlite::fromJSON(content)$records #returns nested df's
+  browser()
+
+  request<-httr::GET(url, httr::accept("application/json"))
+  content<-jsonlite::fromJSON(httr::content(request, "text", encoding = "UTF-8"))
+  content$metaInfo$paging$maxPage
+  records<-content$records #returns nested df's
+  View(records)
 }
